ef0467c55c61c7a6ce604dad619a82b42b2f2f10
jking67 <jking67@asu.edu>
2025-03-02 01:47:09 -0500

    Update server.py
    Imported dependencies like numpy, mp_drawing, mp_pose
    
    Created calculate_angle function
    Created sit_stand_processor function
    
    sit_stand_processor can take in a video or use webcam, process the feed, define the "sit" and "stand" stages, and count the number of reps a person does in the video.

--- src/flask-server/server.py
+++ src/flask-server/server.py
@@ -1,9 +1,122 @@
 from flask import Flask, request
 import cv2
 import os
+import mediapipe as mp
+import numpy as np
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
 
 app = Flask(__name__)
 
+def calculate_angle(a, b, c):
+
+    a = np.array(a) #First
+    b = np.array(b) #Second
+    c = np.array(c) #Third
+
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(radians*180 / np.pi)
+
+    if angle > 180.0:
+        angle = 360 - angle
+
+    return angle
+
+
+def sit_stand_processor(video_path):
+    #VIDEO FEED
+    cap = cv2.VideoCapture(video_path)
+
+    #sit-stand counter
+    counter = 0
+    stage = None
+
+    #Setup mediapipe instance
+    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+
+        while cap.isOpened():
+            ret, frame = cap.read()
+
+            if not ret:
+                break  # End of video
+
+            
+
+            #detect stuff and render
+
+            #recolor image to RBG
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+            #make detections
+            results = pose.process(image)
+            #recolor image to BGR
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            #extract landmarks
+            try:
+                landmarks = results.pose_landmarks.landmark
+
+                #get coordinates
+                lShoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                lHip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x, landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+                rHip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+                lKnee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x, landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+                rKnee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+                lAnkle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x, landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
+                rAnkle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+
+                #calculate angle
+                lAngle = calculate_angle(lHip, lKnee, lAnkle)
+                rAngle = calculate_angle(rHip, rKnee, rAnkle)
+
+                #visualize
+                
+
+                width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                #Left hip
+                cv2.putText(image, str(lAngle),
+                            tuple(np.multiply(lHip, [width, height]).astype(int)),
+                                  cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+                #Right Hip
+                #cv2.putText(image, str(rAngle),
+                 #           tuple(np.multiply(rHip, [width, height]).astype(int)),
+                  #                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+                counter_text = "Count: " + str(counter)
+                cv2.putText(image, counter_text,
+                            tuple(np.multiply(lShoulder, [width * 1.2, height]).astype(int)),
+                                  cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+                #sit-stand counter logic
+                if lAngle <= 150 and rAngle <= 150:
+                    stage = "sit"
+                if lAngle >= 175 and rAngle >= 175 and stage=="sit":
+                    stage = "stand"
+                    counter += 1
+                
+
+                
+
+            except:
+                pass
+
+            #render detections
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                      mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2),
+                                      mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2))
+
+            
+
+            cv2.imshow('Mediapipe Feed', image)
+
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+    return counter
 
 def video_processor(video_path):
 
@@ -72,12 +185,13 @@ def upload():
        filename = video.filename
        video.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
        path_to_file = "uploads/" + filename
-       video_processor(path_to_file)
+       #video_processor(path_to_file)
+       reps = sit_stand_processor(path_to_file)
 
        #############################################################################################################
-       #video.save()
+       
 
-       return video.filename 
+       return video.filename #reps 
     return "failure"
 
 if __name__ == "__main__":
